2508b1370a10de95db60b7895a838fec4c29bde1
LoveIsGrief <LoveIsGrief@users.noreply.github.com>
2017-08-17 00:17:49 +0200

    Lotsa TODOs
    LoveIsGrief/playpen#1 imdb2tmdb

---
+++ imdb2tmdb/main.py
@@ -0,0 +1,22 @@
+"""
+TODO should use https://github.com/celiao/tmdbsimple/
+TODO pip install tmdbsimple
+"""
+
+__author__ = "LoveIsGrief"
+
+
+def main(cmd_args):
+    # TODO: Auth to tmdb
+    # TODO: open CSV
+    # TODO: loop through CSV and search by imdb_id
+    # TODO: add movie to watchlist or favorites
+    pass
+
+
+if __name__ == '__main__':
+    # TODO argument parser
+    # TODO main.py [OPTIONS] <.csv>
+    # TODO --favorites adds movies in CSV to favorites
+    # TODO --watchlist adds movies in CSV to watchlist
+    main(None)
